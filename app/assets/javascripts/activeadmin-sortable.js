4e4fad3fd06a5c30187cbad26ff53842bd74407a
Bastiaan Peters <bastiaan@redwebs.nl>
2017-12-12 18:11:50 +0100

    send csrf along

--- app/assets/javascripts/activeadmin-sortable.js
+++ app/assets/javascripts/activeadmin-sortable.js
@@ -7,10 +7,14 @@
     this.sortable({
       update: function(event, ui) {
         var url = ui.item.find('[data-sort-url]').data('sort-url');
+        var token = $('meta[name="csrf-token"]').attr('content');
 
         $.ajax({
           url: url,
           type: 'post',
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-CSRF-Token', token)
+          },
           data: { position: ui.item.index() + 1 },
           success: function() { window.location.reload() }
         });
